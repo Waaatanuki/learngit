2766a0b40f24f0375dbc03eeb78ead54a208fe6f
Waaatanuki <watanukipublic@gmail.com>
2023-10-17 11:34:39 +0800

    Init

---
+++ src/views/panel/main.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+function bar() {
+  ElMessage.success('panel')
+}
+</script>
+
+<template>
+  <div>Hello Panel!</div>
+  <el-button @click="bar">
+    nihao
+  </el-button>
+</template>
+
+<style scoped></style>
