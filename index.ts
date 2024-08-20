661b1018a08ac32b4e2b077aceac6bcfbf5a147c
Waaatanuki <watanukipublic@gmail.com>
2024-08-20 11:34:55 +0800

    tttt

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@ import { updateRepo } from './git'
 console.log(1111)
 
 await writeFile('./test.txt', String(Date.now()))
-await writeFile('./asset/test.txt', String(Date.now()))
+await writeFile('../asset/test.txt', String(Date.now()))
 // await updateRepo()
